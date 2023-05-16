be04c3f4c81788a00d151887e237176f7f9d4037
Ralphilou Tatoy <ralphilou24@gmail.com>
2023-05-16 16:38:59 -0700

    Add project files.

---
+++ WebApplicationResume/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using WebApplicationResume.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationResume.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //pass context to base class (DbContext)
+        {
+
+        }
+        public DbSet<Project> Categories { get; set; }
+
+    }
+}
---
+++ WebApplicationResume/Models/Project.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace WebApplicationResume.Models
+{
+    public class Project
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [DisplayName("Title")]//Displays Name instead of name when validation is required
+        public string title { get; set; }
+
+        public double gpa { get; set; }
+
+        [Required]
+        [DisplayName("City")]//Displays Name instead of name when validation is required
+        public string city { get; set; }
+
+        [Required]
+        [DisplayName("Diploma")]//Displays Name instead of name when validation is required
+        public string diploma { get; set; }
+
+        [Required]
+        [DisplayName("Content")]//Displays Name instead of name when validation is required
+        public string content { get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
